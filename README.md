6de806561c26fab3fe500aa757cf0056db856141
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2020-08-23 12:47:38 +0200

    Assignment Ready

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# forelesning2
- Dette er et repo med oppgaver til forelesning 2. 
+# Forelesning 2
+Dette er en repetisjon av hva dere kan lage ved bruka av HTML, CSS og JavaScript. Jeg anbefaler dere å fullføre oppgaven med følgende verktøy:
+- VS Code [https://code.visualstudio.com][1]
+- Live serve: Extension i VS Code 
+
+## Oppgave:
+Lag et grensesnitt for js filen som ligger i mappen data og presenter det på best mulig måte på en mobil. ￼￼Dette kan gjøres slik som du slev ønsker, men du må presentere bildet, tittelen, og beskrivelsen i en liste. 
+
+**MERK: Trenger du en forfriskning på kunnskap innfor JavaScript så har jeg skrevet et lite synopsis under**
+
+
+
+[1]:	https://code.visualstudio.com
---
+++ data/posts.js
@@ -0,0 +1,33 @@
+let postes = [
+    {
+        id: 1,
+        image: "https://www.campaniartecard.it/wp-content/uploads/2018/11/castel-nuovo-3.jpg",
+        title: 'First post',
+        description: 'This is the first post in the data'
+    },
+    {
+        id: 2,
+        image: "https://cdn.pixabay.com/photo/2019/07/30/18/32/castel-4373569_960_720.jpg",
+        title: 'Secound post',
+        description: 'This is the secound post in the data'
+    },
+    {
+        id: 3,
+        image: "https://cdn.civitatis.com/italia/andria/galeria/castel-monte.jpg",
+        title: 'Third post',
+        description: 'This is the third post in the data'
+    },
+    {
+        id: 4,
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/0f/e5/2a/67/castel-porrona-relais.jpg",
+        title: 'Fourth post',
+        description: 'This is the fourth post in the data'
+    },    {
+        id: 5,
+        image: "https://media.snl.no/media/138890/standard_compressed_Castel_Sant_Angelo.jpg",
+        title: 'Fift post',
+        description: 'This is the fift post in the data'
+    }
+]
+
+
