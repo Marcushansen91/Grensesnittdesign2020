7fef4e09f38c191877db99d7eccd95473b284392
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2020-08-24 11:38:03 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
-import {getPostes} from '../data/posts.js';
+import { getPostes } from '../data/posts.js';
 
-let container = document.getElementById('container');
 const posts = getPostes(); //Gets postes from data
+
+//DOM connections
+let container = document.getElementById('container');
 
 //Creates a H2 with text
 const createTitle = (title) => {
@@ -10,6 +12,15 @@ const createTitle = (title) => {
     header.appendChild(text);
     return header;
 }
+
+//TODO: Create a img function to present an image
+
+//TODO: Create a description child node
+
+/* TODO: 
+Maby make a card View that is combining all 
+the other functions and creates a nice representation 
+of each item in the database */
 
 //Prints one by one post
 posts.forEach(post => {
